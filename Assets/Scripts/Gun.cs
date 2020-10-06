4409fb4bce2246133dba01cf353a7465f998341c
Manjunatha-b <manjunatha.bairi@gmail.com>
2020-10-06 19:39:19 +0530

    First Update:
    1.) FirstPersonController script instantiates bullets on click of mouse
    2.) bullet destroys objects tagged "enemy" and destroys itself
    3.) bullet doesn't destroy any other object and destroys itself if it
    collides with a non enemy object
    4.) bullet destroys itself if it travels 250f without colliding with
    anything
    5.) before the bullet destroys itself, it calls an explosion particle
    system.
    6.) the explosion particle system is set to destroy itself within 1s of
    its creation to ensure no memory is wasted.
    7.) upon destruction each bullet object calls its parent
    FirstPersonController script's function 'loghit' which prints to console
    that an enemy has been hit.
    
    TODO:
    1.) find a way to spawn enemies at random
    2.) train a base, non moving enemy scenario using SAC

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cam;
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.right = -cam.transform.forward;
+    }
+}
---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class bullet : MonoBehaviour
+{
+    public float speed = 60f;
+    public ParticleSystem bulletFlash;
+    Vector3 previouspos;
+    Vector3 startpos;
+    bool hit;
+    RigidbodyFirstPersonController parent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        previouspos = transform.position;
+        startpos = transform.position;
+        hit = false;
+    }
+
+    public void assignParent(RigidbodyFirstPersonController p1)
+    {
+        parent = p1;
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit herebruh;
+
+        Ray waw = new Ray(previouspos, (this.transform.position - previouspos).normalized);
+        if (Physics.Raycast(waw, out herebruh, (transform.position - previouspos).magnitude) && !hit)
+        {
+            ParticleSystem bruh = Instantiate(bulletFlash);
+            bruh.transform.position = herebruh.point;
+            bruh.transform.forward = herebruh.normal;
+            if (herebruh.transform.tag == "enemy")
+            {
+                herebruh.transform.gameObject.transform.position = new Vector3()
+                parent.loghit();
+            }
+            Destroy(this.gameObject);
+            hit = true;
+        }
+        if (Vector3.Distance(startpos, transform.position) > 250f)
+        {
+            
+            Destroy(this.gameObject);
+        }
+        previouspos = transform.position;
+    }
+}
